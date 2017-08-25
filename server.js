105aaaa05ff1248f1d9ae1d8d99485d4bbf7ba86
ShinyTimes <andr4666@stud.kea.dk>
2017-08-25 12:46:15 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+// Lave en webserver
+var http = require('http');
+
+var server = http.createServer(function (req, res)
+{
+    res.writeHead(200, {'Content-Type': 'text/json'});
+    res.write('{"title" : "Hello Darkness My Old Friend", "text", "I\'ve come to speak with you again"}');
+});
+
+server.listen(3000);
